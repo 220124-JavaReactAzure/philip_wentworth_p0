421a854d0a85268fc0d7b43e419de0329b4c99de
Philip Wentworth <guitartheist@gmail.com>
2022-01-29 18:12:27 -0500

    Updated MyCharacter.toString and refactored design to use constants instead of Statistic objects

--- CharacterManagementSystem/src/com/revature/phil_wentworth/models/MyCharacter.java
+++ CharacterManagementSystem/src/com/revature/phil_wentworth/models/MyCharacter.java
@@ -1,8 +1,8 @@
 package com.revature.phil_wentworth.models;
 
-import java.util.Arrays;
-
 public class MyCharacter {
+	public static final String[] statisticNames = {"Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"};
+	public static final String[] statisticAbbreviations = {"STR","DEX","CON","INT","WIS","CHA"};
 	private int[] statistics;
 	private int id;
 	private int userID;
@@ -12,6 +12,7 @@ public class MyCharacter {
 		this.id = id;
 		this.userID = userID;
 		this.characterName = characterName;
+		this.statistics = new int[6];
 	}
 	
 	public int[] getStatistics() {
@@ -48,7 +49,15 @@ public class MyCharacter {
 
 	@Override
 	public String toString() {
-		return characterName + ": " + Arrays.toString(statistics);
+		StringBuilder returnString = new StringBuilder( characterName + ":\n");
+		for (int i=0; i<statistics.length; i++) {
+			returnString.append(statisticAbbreviations[i] + "\t");
+		}
+		returnString.append("\n");
+		for (int i=0; i<statistics.length; i++) {
+			returnString.append(statistics[i] + "\t");
+		}
+		return returnString.toString();
 	}
 	
 	
