f217bf0449fbe09a752b725b019dbabd669cfb3f
Philip Wentworth <guitartheist@gmail.com>
2022-02-03 18:34:49 -0500

    implemented character_generate

--- CharacterManagementSystem/src/main/java/com/revature/phil_wentworth/menus/my_character_menus/CharacterCompareMenu.java
+++ CharacterManagementSystem/src/main/java/com/revature/phil_wentworth/menus/my_character_menus/CharacterCompareMenu.java
@@ -11,6 +11,7 @@ public class CharacterCompareMenu extends Menu {
 
 	public CharacterCompareMenu(BufferedReader consoleReader, MenuRouter router, MyCharacterService myCharacterService) {
 		super("Character Compare","/character_compare", consoleReader, router);
+		this.myCharacterService = myCharacterService;
 	}
 
 	@Override
